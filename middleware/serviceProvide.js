2df4238dc6cdeb84479997e8eb903cb50fe4c45e
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2024-02-16 13:56:17 +0300

    first commit

--- middleware/serviceProvide.js
+++ middleware/serviceProvide.js
@@ -3,45 +3,45 @@ class Database {
 
   Add(data) {
     data.id = data.length;
-    db.push(data);
+    this.db.push(data);
     return data;
   }
 
   Delete(id) {
-    const length = db.length;
-    db = db.filter((data) => data.id != id);
-    if (db.length == length) throw new Error("Resource Not Found");
+    const length = this.db.length;
+    this.db = this.db.filter((data) => data.id != id);
+    if (this.db.length == length) throw new Error("Resource Not Found");
     return {};
   }
 
   Update(id, data) {
     let index;
     for (let i = 0; i < data.length; i++) {
-      if (db[i].id == id) {
+      if (this.db[i].id == id) {
         index = i;
         break;
       }
     }
     if (index == null) throw new Error("Resource Not FOund");
-    db[index] = data;
-    return db[index];
+    this.db[index] = data;
+    return this.db[index];
   }
 
   GetAll() {
-    return db;
+    return this.db;
   }
   GetById(id) {
-    const data = db.filter((data) => data.id == id);
+    const data = this.db.filter((data) => data.id == id);
     if (data.length == 0) throw new Error("Resource Not Found");
     return data[0];
   }
 }
 
-let db;
+let this.db;
 
 export function provideDatabase() {
-  if (db == null) {
-    db = new Database();
+  if (this.db == null) {
+    this.db = new Database();
   }
-  return db;
+  return this.db;
 }
